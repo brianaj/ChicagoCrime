c74ce65f1fbc7e8003462f02c81ac2b0a7447e74
Hadoop Generic User <hduser@ubuntu.(none)>
2013-06-26 18:53:26 -0500

    housecleaning

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-#     myresult = %x( /home/hduser/wrapper.sh -c #{params[:crime_type]} -h #{params[:hour].to_i} -l #{params[:location_type]})
+#    myresult = %x( /home/hduser/wrapper.sh -c #{params[:crime_type]} -h #{params[:hour].to_i} -l #{params[:location_type]} -a #{params[:is_arrested]?"true":"false"})
   end
 end
